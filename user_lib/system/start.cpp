3f0e6c627fa075dbe6bb8793967a1ccb7f4cca31
bgzerol <1600494183@qq.com>
2026-08-17 15:17:56 +0800

    添加 .agents

--- user_lib/system/start.cpp
+++ user_lib/system/start.cpp
@@ -1,11 +1,7 @@
-#include "debug/foc_open_loop_debug.h"
-#include "devices/led_dev.h"
-
 /**
  * @brief 初始化所有用户模块
  */
 extern "C" void start_init_all(void)
 {
-    foc_open_loop_debug::init();
-    led_dev::init();
+    
 }
